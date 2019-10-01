ac877de9813cbf6ffb81cb8aa21c4b4884c5c8af
unknown <saram2261@gmail.com>
2019-10-01 16:13:16 -0500

    First Commit of IMG Project

---
+++ resources/views/home.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-3 p-5">
+
+            <img src="https://scontent-ort2-2.cdninstagram.com/vp/63791168358069753f0964d898d68a39/5E1E24FA/t51.2885-15/e35/c0.84.1440.1440a/s150x150/70231060_147750756438503_8233797372296025610_n.jpg?_nc_ht=scontent-ort2-2.cdninstagram.com&_nc_cat=111" class="rounded-circle">
+        </div>
+
+            <div class="col-9 pt-5">
+<div><h1>#Puppy</h1></div>
+            <div class="d-flex">
+                <div class="pr-5"><strong>120,500,572</strong> posts</div>
+                <div class="pr-5"><strong>3746</strong> followers</div>
+                <div class="pr-5"><strong>479</strong> following</div>
+            </div>
+
+            </div>
+    </div>
+
+
+        </div>
+    </div>
+</div>
+@endsection

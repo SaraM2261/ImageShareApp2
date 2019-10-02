4b36968b373ec6aa72a836167f1b53d1c5b2cdf0
unknown <saram2261@gmail.com>
2019-10-02 12:18:21 -0500

    Handle Images and Show to Auth User Profile Page

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -5,21 +5,21 @@
     <div class="row">
         <div class="col-3 p-5">
 
-            <img src="/Images/hughjackman-profileimg.png" class="rounded-circle">
+            <img src="https://pbs.twimg.com/profile_images/843511681905033216/uK-HR5g1_400x400.jpg" class="rounded-circle" height="150"  >
         </div>
 
             <div class="col-9 pt-5">
              <div class="d-flex justify-content-between align-items-baseline">
                 <h1>{{$user->username}}</h1>
-                 <a href="#">Add New Post</a>
+                 <a href="/p/create">Add New Post</a>
 
              </div>
             <div class="d-flex">
-                <div class="pr-5"><strong>2,243</strong> posts</div>
+                <div class="pr-5"><strong>{{ $user->posts->count() }}</strong> posts</div>
                 <div class="pr-5"><strong>2.81m</strong> followers</div>
                 <div class="pr-5"><strong>0</strong> following</div>
             </div>
-                <div class="pt-4">{{$user->profile->title}}</div>
+                <div class="pt-4"><strong>{{$user->profile->title}}</strong></div>
                 <div>{{$user->profile->description}}</div>
               <!-- hide hugh jackman url and link
                 <div><a href="https://l.instagram.com/?u=http%3A%2F%2FHughJackmanTheShow.com%2F&e=ATOTKXczIOy6Qem-YGb261KGmw_rgAlu8E9QpYp23gaM7NWBFiVaoUtQ0_i2-69L_cE31QE83t18YcPQ_Q">HughJackmanTheShow.com</a></div>
@@ -29,17 +29,17 @@
     </div>
 
         <div class="row pt-5">
-         <div class="col-4">
-             <img src="https://scontent-ort2-1.cdninstagram.com/vp/c0eaec3beb3e6952b717d8b4e026f6f5/5E395766/t51.2885-15/e35/s240x240/21576522_1684490954940770_9062059499078025216_n.jpg?_nc_ht=scontent-ort2-1.cdninstagram.com&_nc_cat=102" class="w-100" />
-         </div>
-            <div class="col-4">
-                <img src="https://scontent-ort2-1.cdninstagram.com/vp/c0eaec3beb3e6952b717d8b4e026f6f5/5E395766/t51.2885-15/e35/s240x240/21576522_1684490954940770_9062059499078025216_n.jpg?_nc_ht=scontent-ort2-1.cdninstagram.com&_nc_cat=102" class="w-100" />
-            </div>
-            <div class="col-4">
-                <img src="https://scontent-ort2-1.cdninstagram.com/vp/c0eaec3beb3e6952b717d8b4e026f6f5/5E395766/t51.2885-15/e35/s240x240/21576522_1684490954940770_9062059499078025216_n.jpg?_nc_ht=scontent-ort2-1.cdninstagram.com&_nc_cat=102" class="w-100" />
-            </div>
 
-         </div>
+            @foreach($user->posts as $post)
+                <div class="col-4 pb-4">
+                    <img src="/storage/{{ $post->image }}" class="w-100" >
+
+                </div>
+
+            @endforeach
+
+
+
         </div>
     </div>
 

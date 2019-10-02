1edcd7ff03f2e909f8286dee8a0052cc8db4219d
unknown <saram2261@gmail.com>
2019-10-01 23:56:44 -0500

    Make and Associtate User ID with Profiles

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,14 +9,18 @@
         </div>
 
             <div class="col-9 pt-5">
-<div><h1>thehughjackman</h1></div>
+<div><h1>{{$user->username}}</h1></div>
             <div class="d-flex">
                 <div class="pr-5"><strong>2,243</strong> posts</div>
                 <div class="pr-5"><strong>2.81m</strong> followers</div>
                 <div class="pr-5"><strong>0</strong> following</div>
             </div>
-                <div class="pt-4"><strong>Hugh Jackman</strong></div>
+                <div class="pt-4">{{$user->profile->title}}</div>
+                <div>{{$user->profile->description}}</div>
+              <!-- hide hugh jackman url and link
                 <div><a href="https://l.instagram.com/?u=http%3A%2F%2FHughJackmanTheShow.com%2F&e=ATOTKXczIOy6Qem-YGb261KGmw_rgAlu8E9QpYp23gaM7NWBFiVaoUtQ0_i2-69L_cE31QE83t18YcPQ_Q">HughJackmanTheShow.com</a></div>
+              -->
+                <div><a href="#">{{$user->profile->url }}</a></div>
             </div>
     </div>
 
